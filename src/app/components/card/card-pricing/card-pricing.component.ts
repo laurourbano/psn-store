a598064df7d6e2944bcd6484234b103579adbf3c
Lauro Otavio Urbano <lauro@outlook.com.br>
2024-01-18 01:41:20 -0300

    adicionando o card-pricing

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrl: './card-pricing.component.scss',
+})
+export class CardPricingComponent {
+  @Input() gameType: string = 'Digital PS4';
+  @Input() gamePrice: string = 'R$ 259.99';
+}
